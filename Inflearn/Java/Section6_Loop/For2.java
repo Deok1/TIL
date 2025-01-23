ecfa0f5eea73b03db7a296ae6ad36b9f59d93feb
Deok_Il Kim <88528703+Deok1@users.noreply.github.com>
2025-01-23 13:43:07 +0900

    Add files via upload

---
+++ Inflearn/Java/Section6_Loop/For2.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class For2 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int endNum = 10;
+
+
+        for (int i = 1; i <= endNum; i++) {
+            sum += i;
+            System.out.print("sum = " + sum + " ");
+            System.out.println("i = " + i);
+        }
+    }
+}
