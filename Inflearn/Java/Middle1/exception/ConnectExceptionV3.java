0213d46448176f09efb3f59e58cc4ca442a4527b
Deok_Il Kim <88528703+Deok1@users.noreply.github.com>
2025-03-08 10:45:57 +0900

    Add files via upload

---
+++ Inflearn/Java/Middle1/exception/ConnectExceptionV3.java
@@ -0,0 +1,15 @@
+package exception.ex3.exception;
+
+public class ConnectExceptionV3 extends NetworkClientExceptionV3 {
+
+    private final String address;   //어느 서버의 연결을 실패했는지, 서버의 주소를 담을 변수
+
+    public ConnectExceptionV3(String address, String message) {
+        super(message);
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
