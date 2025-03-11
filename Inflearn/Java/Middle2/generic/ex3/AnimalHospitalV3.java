f9f554a49c3c6fbb498122c12bb78dfc6862a3de
Deok_Il Kim <88528703+Deok1@users.noreply.github.com>
2025-03-11 21:07:29 +0900

    Add files via upload

--- Inflearn/Java/Middle2/generic/ex3/AnimalHospitalV3.java
+++ Inflearn/Java/Middle2/generic/ex3/AnimalHospitalV3.java
@@ -2,11 +2,11 @@ package generic.ex3;
 
 import generic.animal.Animal;
 
-public class AnimalHospitalV1 {
+public class AnimalHospitalV3<T extends Animal> {
 
-    private Animal animal;
+    private T animal;
 
-    public void set(Animal animal) {
+    public void set(T animal) {
         this.animal = animal;
     }
 
@@ -16,7 +16,7 @@ public class AnimalHospitalV1 {
         animal.sound();
     }
 
-    public Animal getBigger(Animal target) {
+    public T getBigger(T target) {
         return animal.getSize() > target.getSize() ? animal : target;
     }
 }
