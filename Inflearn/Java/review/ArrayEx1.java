cdcd8259383323bf33891ecb2669f15d89ddd657
Deok_Il Kim <88528703+Deok1@users.noreply.github.com>
2025-03-19 10:17:09 +0900

    Add files via upload

--- Inflearn/Java/review/ArrayEx1.java
+++ Inflearn/Java/review/ArrayEx1.java
@@ -1,22 +1,16 @@
-package array.ex;
+package collection.list.test.ex1;
 
 public class ArrayEx1 {
     public static void main(String[] args) {
-
         int[] students = {90, 80, 70, 60, 50};
-        int count = 0;
-        int total = 0;
-        double average = 0;
 
-        for (int student : students) {
-            total += student;
-            count++;
+        int total = 0;
+        for (int i = 0; i < students.length; i++) {
+            total += students[i];
         }
 
-        average = (double) total/count;
-
-
-        System.out.println("점수 총합: " + total);
-        System.out.println("점수 평균: " + average);
+        double average = (double) total / students.length;
+        System.out.println("점수 총합 = " + total);
+        System.out.println("점수 평균 = " + average);
     }
 }
